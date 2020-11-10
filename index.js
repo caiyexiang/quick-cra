253e812f3c1b9a0dcbb55a912d0c6de3aaa9bdae
wuhao <hwu78035@outlook.com>
2020-11-10 23:08:29 +0800

    [feat]: 变更文件结构，为添加多种模板做准备 [version]: 1.1.2

--- index.js
+++ index.js
@@ -1,63 +1,17 @@
 #! /usr/bin/env node
-const { execSync: RawExecSync } = require("child_process");
-const fs = require('fs');
-const execSync = (command) => RawExecSync(command, { stdio: [0, 1, 2] });
+const quickCra = require('./quick-cra');
 
-const projectPath = process.argv[2];
+/** 快速创建的模板名称 */ 
+const template = process.argv[2];
+/** 快速创建的项目名称 */
+const projectName = process.argv[3];
 
-/**
- * 读取json
- * @param {string} path 文件路径
- */
-const readJsonSync = (path) => {
-  const fileContent = fs.readFileSync(path);
-  return JSON.parse(fileContent);
+const funcMap = {
+  cra: quickCra,
 };
 
-/**
- * 写入Json
- * @param {string} path 写入的文件路径
- * @param {any} data 写入的data
- */
-const writeJsonSync = (path, data) => {
-  const fileContent = JSON.stringify(data, undefined, 2);
-  fs.writeFileSync(path, fileContent);
-};
-
-const main = () => {
-  console.time('创建React应用');
-  console.log('---- 开始创建 ----');
-  // 用cra创建应用
-  execSync(`yarn create react-app ${projectPath} --template typescript`);
-
-  // 添加开发依赖
-  execSync(
-    `cd ${projectPath} && yarn add -D commitizen customize-cra cz-conventional-changelog eslint eslint-config-airbnb-typescript eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-prettier eslint-plugin-react eslint-plugin-react-hooks prettier react-app-rewired`
-  );
-
-  // 修改package.json
-  const packageJson = readJsonSync(`${projectPath}/package.json`);
-  packageJson.config = {
-    commitizen: {
-      path: "cz-conventional-changelog",
-    },
-  };
-  packageJson.scripts.commit = "./node_modules/.bin/git-cz";
-  writeJsonSync(`${projectPath}/package.json`, packageJson);
-
-  // 复制基础配置文件
-  const configFiles = fs.readdirSync(`${__dirname}/configs`);
-  for (const filename of configFiles) {
-    fs.copyFileSync(`${__dirname}/configs/${filename}`, `./${projectPath}/${filename}`);
-  }
-
-  // 添加git记录
-  execSync(
-    `cd ${projectPath} && git commit -a -m "Project configuration initialization."`
-  );
-
-  console.log("---- 完成创建 ----");
-  console.timeEnd("创建React应用");
-};
-
-main();
+if (funcMap[template]) {
+  funcMap[template](projectName);
+} else {
+  console.error('不存在对应模板');
+}
---
+++ quick-cra/index.js
@@ -0,0 +1,64 @@
+#! /usr/bin/env node
+const { execSync: RawExecSync } = require("child_process");
+const fs = require('fs');
+const execSync = (command) => RawExecSync(command, { stdio: [0, 1, 2] });
+
+/**
+ * 读取json
+ * @param {string} path 文件路径
+ */
+const readJsonSync = (path) => {
+  const fileContent = fs.readFileSync(path);
+  return JSON.parse(fileContent);
+};
+
+/**
+ * 写入Json
+ * @param {string} path 写入的文件路径
+ * @param {any} data 写入的data
+ */
+const writeJsonSync = (path, data) => {
+  const fileContent = JSON.stringify(data, undefined, 2);
+  fs.writeFileSync(path, fileContent);
+};
+
+/**
+ * 快速使用cra创建react应用
+ * 
+ * @param {string} projectPath 项目名称
+ */
+module.exports = function (projectPath) {
+  console.time('创建React应用');
+  console.log('---- 开始创建 ----');
+  // 用cra创建应用
+  execSync(`yarn create react-app ${projectPath} --template typescript`);
+
+  // 添加开发依赖
+  execSync(
+    `cd ${projectPath} && yarn add -D commitizen customize-cra cz-conventional-changelog eslint eslint-config-airbnb-typescript eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-prettier eslint-plugin-react eslint-plugin-react-hooks prettier react-app-rewired`
+  );
+
+  // 修改package.json
+  const packageJson = readJsonSync(`${projectPath}/package.json`);
+  packageJson.config = {
+    commitizen: {
+      path: "cz-conventional-changelog",
+    },
+  };
+  packageJson.scripts.commit = "./node_modules/.bin/git-cz";
+  writeJsonSync(`${projectPath}/package.json`, packageJson);
+
+  // 复制基础配置文件
+  const configFiles = fs.readdirSync(`${__dirname}/configs`);
+  for (const filename of configFiles) {
+    fs.copyFileSync(`${__dirname}/configs/${filename}`, `./${projectPath}/${filename}`);
+  }
+
+  // 添加git记录
+  execSync(
+    `cd ${projectPath} && git commit -a -m "Project configuration initialization."`
+  );
+
+  console.log("---- 完成创建 ----");
+  console.timeEnd("创建React应用");
+};
