319bda696888327f363bc77d6c4acc6c3813f3ed
wuhao <wuhao.igno@bytedance.com>
2020-11-11 22:05:43 +0800

    [fix]: 修复base和koa模板index.ts配反的问题 [version]: 1.2.2

--- quick-koa/src/index.ts
+++ quick-koa/src/index.ts
@@ -1,1 +1,20 @@
-console.log('Start coding.');
+import Koa from "koa";
+import KoaBodyParser from "koa-bodyparser";
+import router from "./routes";
+
+const app = new Koa();
+
+app
+  .use(
+    async (ctx, next): Promise<void> => {
+      const start = Date.now();
+      await next();
+      const end = Date.now();
+      console.log(`处理时间：${end - start}ms`);
+    }
+  )
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .use(KoaBodyParser())
+  .listen(5000);
+console.log("server run at http://localhost:5000");
